2d7dea12717e6fc848b9b1d7cd44f11e91c2b89c
TSK2302 <sayant2001@gmail.com>
2026-04-17 18:22:07 +0530

    auto deploy test 22

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>React + Vite deployed on EC2 i made changes, now made git hub auto deploy onvce cod e pushed</h1>
-      <p>If you see this live, you did it right.</p>
+      <p>If you see this live, you did it right. this is auto depoly chcecking is ./sh file deployed my app </p>
     </div>
   );
 }
