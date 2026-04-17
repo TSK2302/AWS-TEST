a39df6256fc6e25c14f37ef65eba941e7c602f13
TSK2302 <sayant2001@gmail.com>
2026-04-17 18:05:07 +0530

    auto deploy 1st code push

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>React + Vite deployed on EC2 i made changes</h1>
+      <h1>React + Vite deployed on EC2 i made changes, now made git hub auto deploy onvce cod e pushed</h1>
       <p>If you see this live, you did it right.</p>
     </div>
   );
