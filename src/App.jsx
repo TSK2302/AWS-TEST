8415f96950863b904a44a88253ceb0441a0ae0d2
TSK2302 <sayant2001@gmail.com>
2026-04-17 17:34:20 +0530

    making changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>React + Vite deployed on EC2</h1>
+      <h1>React + Vite deployed on EC2 i made changes</h1>
       <p>If you see this live, you did it right.</p>
     </div>
   );
