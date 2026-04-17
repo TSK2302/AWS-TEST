ba1052e4869b9c7c86b25d818059a15632f5aa48
TSK2302 <sayant2001@gmail.com>
2026-04-17 17:17:05 +0530

    aws setup ci/cd

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>React + Vite deployed on EC2</h1>
+      <p>If you see this live, you did it right.</p>
+    </div>
+  );
+}
